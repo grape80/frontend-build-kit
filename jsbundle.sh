5633cdeb8d6fe2a0dc6bad0dc918c3c38813abd8
hachirog <hachiro@grape80.com>
2023-06-19 15:08:15 +0000

    Update files.

--- jsbundle.sh
+++ jsbundle.sh
@@ -9,5 +9,5 @@ dest=$(echo $1 | cut -d '=' -f 2)
 entryJS=$2
 bundleJS=$(echo $entryJS | sed -e "s/^$srcDir/$dest/" -e "s/\\.js$/\\.bundle.js/")
 
-mkdir -p `dirname $bundleJS`
+mkdir -p $(dirname $bundleJS)
 esbuild $entryJS --bundle --sourcemap --outfile=$bundleJS
